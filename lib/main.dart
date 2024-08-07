5921eac58f9c7d05465cca52957ed5d3088fda84
Rohan-Prajapati-2000 <rohanpra2000@gmail.com>
2024-08-07 23:33:01 +0530

    some modification for send schedule email

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,3 @@
-// import 'dart:convert';
-//
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:flutter/material.dart';
-//
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:get/get.dart';
-// import 'package:get_storage/get_storage.dart';
-// import 'package:practice/new_user_or_update_user_screen/model/subscription_model.dart';
-// import 'package:practice/utils/theme/theme.dart';
-// import 'firebase_options.dart';
-// import 'home_screen/home.dart';
-// import 'package:cron/cron.dart';
-// import 'package:http/http.dart' as http;
-//
-// void main() async{
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-//   );
-//
-//
-//   var cron = Cron();
-//   cron.schedule(Schedule.parse('06 17 * * *'), () async {
-//
-//   });
-//
-//
-//   runApp(const MyApp());
-// }
-//
-//
-// Future<void> sendEmail(SubscriptionModel subscription) async{
-//   var servideId = 'service_9phdz4c';
-//   var templateId = 'template_ehoiaoh';
-//   var userId = 'D5RTA0HOz54XcZTPQ';
-//
-//   var url = Uri.parse('https://api.emailjs.com/api/v1.0/email/send');
-//
-//   var body = jsonEncode({
-//     'service_id' : servideId,
-//     'user_id' : userId,
-//     'template_id' : templateId,
-//     'template_params' : {
-//       'subject' : 'Subscription Expiry Reminder',
-//       'message' : 'The subscription is expiring in 15 days.',
-//       'to' : 'rohanprajapati5212@gmail.com'
-//     },
-//   });
-//
-//   try{
-//     var response =  await http.post(
-//       url,
-//       headers: {
-//         'origin' : 'http://localhost',
-//         'Content-Type' : 'application/json'
-//       },
-//       body: body,
-//     );
-//
-//     if(response.statusCode == 200){
-//       print('Email sent Successfully!');
-//     } else {
-//       print('Failed to send email. Status code: ${response.statusCode}');
-//       print('Response body: ${response.body}');
-//     }
-//   } catch (e){
-//     print('Error sending email: $e');
-//   }
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return GetMaterialApp(
-//       title: 'Flutter Demo',
-//       themeMode: ThemeMode.system,
-//       theme: SAppTheme.lightTheme,
-//       darkTheme: SAppTheme.darkTheme,
-//       home: MyHomePage(),
-//     );
-//   }
-// }
-//
-
-
 import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -108,7 +19,7 @@ void main() async {
   );
 
   var cron = Cron();
-  cron.schedule(Schedule.parse('35 13 * * *'), () async {
+  cron.schedule(Schedule.parse('22 23 * * *'), () async {
     await checkSubscriptions();
   });
 
@@ -134,35 +45,60 @@ Future<void> checkSubscriptions() async {
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   // Fetch the client_details collection
-  QuerySnapshot querySnapshot = await firestore.collection('client_details').get();
-// print(querySnapshot);
+  QuerySnapshot querySnapshot =
+      await firestore.collection('client_details').get();
+
   for (var doc in querySnapshot.docs) {
     var data = doc.data() as Map<String, dynamic>?;
+    String startDateString = data?['Date'];
+    String email = data?['Contact Email'];
+    String name = data?['Contact Person'];
+    DateTime startDate = DateTime.parse(startDateString);
 
-    // Check if the data is not null and contains 'Subscriptions' field
-    if (data != null && data.containsKey('Subscriptions')) {
-      List<dynamic> subscriptions = data['Subscriptions'];
+    // print('Document Id: ${doc.id}');
+    // print(email);
+    // print(name);
 
-      for (var subscription in subscriptions) {
-        String validity = subscription['Validity'];
-        // Check if validity is equal to '15 days'
-        try {
-          if (validity == '15 days') {
-            // Call the function to send email
-            await sendEmail();
-            print('sent email for document ID: ${doc.id} ');
-          }
-        } catch (e) {
-          print('Error checking subscription for document ID: ${doc.id}, error: $e');
+    var subscriptions = data?['Subscriptions'];
+    // String serviceName = subscriptions['Product Title'];
+    for (var subscription in subscriptions) {
+      var subscriptionMap = subscription as Map<String, dynamic>;
+      var validity = subscriptionMap['Validity'];
+      var serviceName = subscriptionMap['Product Title'];
+
+      // print(serviceName);
+
+      int durationInDays = parseValidityToDays(validity);
+      DateTime endDate = startDate.add(Duration(days: durationInDays));
+      int remainingDays = endDate.difference(DateTime.now()).inDays;
+      // print("End Date: $endDate");
+      // print("Remaining Days: $remainingDays");
+      // print("Validity is: ${validity ?? 'No validity data is available'}");
+      try {
+        if (remainingDays == 0) {
+          await sendExpiredEmail();
+          print('Expiry Email Send');
+        } else if(remainingDays == 7){
+          await sendRenewalReminderEmail(email, name, serviceName, endDate);
+          print("Renewal Reminder Email Sent");
         }
+      } catch (e) {
+        print('Error $e');
       }
-    } else {
-      print('No Subscriptions field or data is null for document ID: ${doc.id}');
     }
   }
 }
 
-Future<void> sendEmail() async {
+int parseValidityToDays(String validity) {
+  if (validity.contains('month')) {
+    int months = int.parse(validity.split(' ')[0]);
+    int result = months * 30;
+    return result; // Approximate conversion
+  }
+  return 0;
+}
+
+Future<void> sendExpiredEmail() async {
   var serviceId = 'service_rr0yw8y';
   var templateId = 'template_ehoiaoh';
   var userId = 'D5RTA0HOz54XcZTPQ';
@@ -174,14 +110,26 @@ Future<void> sendEmail() async {
     'user_id': userId,
     'template_id': templateId,
     'template_params': {
-      'subject': 'Subscription Expiry Reminder',
-      'message': "Dear Sir/Mam,\n"
-          "Thank you for purchasing - we really appreciate you placing your trust in us.\n\n"
-          "We've been designing our products with business needs like yours in mind."
-          "The specific product you've purchased is one of our best sellers. We hope it'll live up to your expectations."
-          "\n"
-          "Should you encounter any issues or have any questions, our support team is available"
-          "from 10:00am to 06:00pm IST at support@helptogethergroup.com or +91 9634644622",
+      'subject': 'Your Service Plan Has Expired',
+      'message': "Dear Client's Name\n\n"
+          "We hope the message finds you well. We wanted to inform you that your [Product/Service] plan"
+          " with Help Together Group has expired as of Today\n\n"
+          "Service Details: \n\n"
+          "Service Name: [Product/Service]"
+          "Expiration Date: [Expiration Date]\n"
+          "We value your association with us and would love to continue providing you with our services."
+          "To avoid any disruption and resume your access to the encourage you to renew your service plan.\n\n"
+          "Renewal Process:\n"
+          "To renew your service contact us.\n\n"
+          "Should you have any questions or need assistance, our support team is available"
+          "from 10:00am to 06:00pm IST. You can reach us at support@helptogethergroup.com or +91 9634644622\n\n"
+          "Thank you for your time and consideration. We hope to continue to serving you.\n\n"
+          "Best Regards,\n\n"
+          "Support Team\n"
+          "Help Together Group\n\n"
+          "Important Contacts:\n"
+          "For Complaints/Support:\n"
+          "(+91) 96346 44622 | support@helptogether.co.in ",
       'to': 'rohanprajapati5212@gmail.com'
     },
   });
@@ -197,7 +145,128 @@ Future<void> sendEmail() async {
     );
 
     if (response.statusCode == 200) {
-      print('Email sent Successfully!');
+      // print('Email sent Successfully!');
+    } else {
+      print('Failed to send email. Status code: ${response.statusCode}');
+      print('Response body: ${response.body}');
+    }
+  } catch (e) {
+    print('Error sending email: $e');
+  }
+}
+
+Future<void> sendRenewalReminderEmail(String email, String name, String service, var endDate) async {
+  if(email == null || email.isEmpty || name == null || name.isEmpty || service==null || service.isEmpty){
+    print('One of the parameters is null or empty');
+    return;
+  }
+  var serviceId = 'service_rr0yw8y';
+  var templateId = 'template_ehoiaoh';
+  var userId = 'D5RTA0HOz54XcZTPQ';
+
+  var url = Uri.parse('https://api.emailjs.com/api/v1.0/email/send');
+
+  var body = jsonEncode({
+    'service_id': serviceId,
+    'user_id': userId,
+    'template_id': templateId,
+    'template_params': {
+      'subject': 'Renewal Reminder: Continue Enjoying Our Services',
+      'message': "Dear $name,\n\n"
+
+          "We hope this message finds you well. As your current service period is "
+          "nearing its end, we wanted to remind you about the upcoming renewal of"
+          " your $service with Help Together Group.\n\n"
+
+          "Service Details:\n\n"
+
+          "Service Name: $service\n"
+          "Renewal Date: $endDate\n"
+          "We’ve enjoyed partnering with you and hope you’ve found our services "
+          "beneficial. Renewing your service will ensure you continue to receive"
+          " uninterrupted access to the features and benefits you’ve "
+          "come to rely on.\n\n"
+          "Renewal Process:\n"
+          "To renew your service contact us.\n\n"
+          "If you have any questions or need assistance with the renewal process, our support team is available"
+          "from 10:00am to 06:00pm IST. You can reach us at support@helptogethergroup.com or +91 9634644622.\n\n"
+          "Thank you for your continued trust in Help Together Group. We look forward to serving you in the future.\n\n"
+          "Warm regards,\n\n"
+          "Support Team\n"
+          "Help Together Group\n\n"
+          "Important Contacts:\n"
+          "For Complaints / Support:\n"
+          "(+91) 96346 44622 | support@helptogether.co.in",
+      'to': '$email'
+    },
+  });
+
+  try {
+    var response = await http.post(
+      url,
+      headers: {
+        'origin': 'http://localhost',
+        'Content-Type': 'application/json'
+      },
+      body: body,
+    );
+
+    if (response.statusCode == 200) {
+      // print('Email sent Successfully!');
+    } else {
+      print('Failed to send email. Status code: ${response.statusCode}');
+      print('Response body: ${response.body}');
+    }
+  } catch (e) {
+    print('Error sending email: $e');
+  }
+}
+
+Future<void> sendRenewalConfirmationEmail() async {
+  var serviceId = 'service_rr0yw8y';
+  var templateId = 'template_ehoiaoh';
+  var userId = 'D5RTA0HOz54XcZTPQ';
+
+  var url = Uri.parse('https://api.emailjs.com/api/v1.0/email/send');
+
+  var body = jsonEncode({
+    'service_id': serviceId,
+    'user_id': userId,
+    'template_id': templateId,
+    'template_params': {
+      'subject': ' Your Service Renewal is Confirmed',
+      'message': "Dear Client's Name\n\n"
+          "We are pleased to inform you that your [Product/Service] with Help Together Group has been successfully renewed.\n\n"
+          "Renewal Details: \n\n"
+          "Service Name: [Product/Service]\n"
+          "New Service Period: [Start Date] to [End Date]\n"
+          "Thank you for continuing to trust us with your business needs. We are committed to providing you with "
+          "exceptional service and support throughout this new service period.\n\n"
+          "If you have any questions or need assistance, our support team is available"
+          "from 10:00am to 06:00pm IST. You can reach us at support@helptogethergroup.com or +91 9634644622\n\n"
+          "Thank you once again for your continued partnership. We look forward to serving you.\n\n"
+          "Warm Regards,\n\n"
+          "Support Team\n"
+          "Help Together Group\n\n"
+          "Important Contacts:\n"
+          "For Complaints/Support:\n"
+          "(+91) 96346 44622 | support@helptogether.co.in ",
+      'to': 'rohanprajapati5212@gmail.com'
+    },
+  });
+
+  try {
+    var response = await http.post(
+      url,
+      headers: {
+        'origin': 'http://localhost',
+        'Content-Type': 'application/json'
+      },
+      body: body,
+    );
+
+    if (response.statusCode == 200) {
+      // print('Email sent Successfully!');
     } else {
       print('Failed to send email. Status code: ${response.statusCode}');
       print('Response body: ${response.body}');
@@ -208,3 +277,121 @@ Future<void> sendEmail() async {
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Timestamp dateTimestamp = data?['Date'] as Timestamp;
+// DateTime startDate = dateTimestamp.toDate();
+// print('Start date for document ID ${doc.id}: $startDate');
+
+// for (var subscription in subscriptions) {
+//   String validity = subscription['Validity'];
+//   int durationInDays = parseValidityToDays(validity);
+//
+//   DateTime endDate = startDate.add(Duration(days: durationInDays));
+//   int remainingDays = endDate.difference(DateTime.now()).inDays;
+
+// Print the parsed validity duration and remaining days
+// print('Document ID: ${doc.id}');
+// print('Parsed Duration in Days: $durationInDays');
+// print('Remaining Days: $remainingDays');
+
+// try {
+//   if (remainingDays == 15) {
+//     // Call the function to send email
+//     await sendEmail();
+//     print('Sent email for document ID: ${doc.id}');
+//   }
+// } catch (e) {
+//   print('Error checking subscription for document ID: ${doc.id}, error: $e');
+// }
+// }
+
+// print('Document data: $data}');
+
+//   var subscriptions = data['Subscriptions'];
+//   print('Subscriptions: ${subscriptions ?? 'No Subscriptions data abailable'}');
+//
+// } else{
+// print('No data available for document id: ${doc.id}');
+// }
+
+// if(subscriptions != null && subscriptions.isNotEmpty){
+// print('Document Id: ${doc.id}');
+// for(var subscription in subscriptions){
+// var subscriptionMap = subscription as Map<String, dynamic>;
+// var validity = subscriptionMap['Validity'];
+// print("Validity is: ${validity ?? 'No validity data is available'}");
+// }
+// } else{
+// print('Document Id: ${doc.id}');
+// print('No Subscriptions data available');
+// }
